5fb7d315d8a1975f838ff08b96dbfc80183f9cf6
Aaron J Harvey <harvey.aaronj@gmail.com>
2020-04-07 01:00:46 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,15 @@
 class Dog 
 def name=(dog_name)
-    this_dogs_name = dog_name
+    @this_dogs_name = dog_name
   end
  
   def name
-    this_dogs_name
+    @this_dogs_name
   end
 end
 
-fido = Dog.new 
-fido.name = "Fido"
-fido.name 
+class Dog 
+  def bark
+    puts "woof!"
+  end
+end
